c2c9a40087ae00f82abfeabd0ed5464a392d8772
JirkaSvobo <79328364+JirkaSvobo@users.noreply.github.com>
2022-09-28 22:07:31 +0200

    mapy rano a vecer

--- streamlit_APP.py
+++ streamlit_APP.py
@@ -12,7 +12,7 @@ import plotly.express as px
 # #####################################
 
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
-query = """SELECT
+query_morning = """SELECT
                 start_station_latitude as lat,
                 start_station_longitude as lon
             FROM edinburgh_bikes
@@ -20,7 +20,16 @@ query = """SELECT
             LIMIT 100000
         """
 
-df_bikes = pd.read_sql(sql=query, con=engine)
+query_afternoon = """SELECT
+                start_station_latitude as lat,
+                start_station_longitude as lon
+            FROM edinburgh_bikes
+            WHERE hour(started_at) BETWEEN 15 AND 19
+            LIMIT 100000
+        """
+
+df_bikes_morning = pd.read_sql(sql=query_morning, con=engine)
+df_bikes_afternoon = pd.read_sql(sql=query_afternoon, con=engine)
 
 # #####################################
 # vizualizace
@@ -31,8 +40,11 @@ st.title('Moje prvni apka')
 page = st.sidebar.radio('Select page',['Mapa','Thomson'])
 
 if page == 'Mapa':
-    st.write('Mapa pouzivani sdilenych kol v Edinburgu')
-    st.map(df_bikes)
+    st.header('Mapa pouzivani sdilenych kol v Edinburgu')
+    st.write('Pocatecni stanice rano 6 az 9')
+    st.map(df_bikes_morning)
+    st.write('Pocatecni stanice odpoledne 15 az 19')
+    st.map(df_bikes_afternoon)
 
 
 if page == 'Thomson':
