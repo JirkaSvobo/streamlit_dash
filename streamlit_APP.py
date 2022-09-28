a6a5359cdf67f4394a211f6dc45e1b8431f6a9f7
JirkaSvobo <79328364+JirkaSvobo@users.noreply.github.com>
2022-09-28 19:26:45 +0200

    pridani radio botonu

--- streamlit_APP.py
+++ streamlit_APP.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title('Moje prvni apka')
 
-st.write('toto je prvni apka')
+
+page = st.sidebar.radio('select page',['Test','Thomson'])
+
+if page == 'Test':
+    st.write('toto je prvni apka')
+
+if page == 'Thomson':
+    st.write('Thomson sampling')
